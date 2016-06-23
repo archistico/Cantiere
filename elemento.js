df4aeef8e07947b1f4be67da0bf08485d906832c
archistico <info@archistico.com>
2016-06-23 15:32:06 +0200

    bind variabili e funzionamento classi

--- elemento.js
+++ elemento.js
@@ -4,12 +4,16 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 var Elemento = (function () {
-    function Elemento(nome, costo) {
+    function Elemento(nome, costo, quantita) {
         this.nome = nome;
         this.costo = costo;
+        this.quantita = quantita;
     }
     Elemento.prototype.visualizza = function () {
-        return this.nome + " - " + this.costo;
+        return "nome: " + this.nome + " | costo: " + this.costo + " | quantita: " + this.quantita;
+    };
+    Elemento.prototype.aggiungi = function () {
+        this.quantita = this.quantita + 1;
     };
     return Elemento;
 }());
@@ -28,14 +32,14 @@ var LavoratoreTipo;
 ;
 var Lavoratore = (function (_super) {
     __extends(Lavoratore, _super);
-    function Lavoratore(nome, costo, tipologia) {
-        _super.call(this, nome, costo);
+    function Lavoratore(nome, costo, quantita, tipologia) {
+        _super.call(this, nome, costo, quantita);
         this.tipologia = tipologia;
     }
     Lavoratore.prototype.visualizza = function () {
-        return this.nome + " - " + this.costo + " - " + LavoratoreTipo[this.tipologia];
+        return _super.prototype.visualizza.call(this) + " | tipologia: " + LavoratoreTipo[this.tipologia];
     };
     return Lavoratore;
 }(Elemento));
-var emilie = new Elemento("Emilie", 10.50);
-var emilieLav = new Lavoratore("Emilie", 10.50, LavoratoreTipo.Architetto);
+var emilie = new Elemento("Emilie", 10.50, 1);
+var emilieLav = new Lavoratore("Emilie", 10.50, 1, LavoratoreTipo.Architetto);
--- elemento.ts
+++ elemento.ts
@@ -1,35 +1,42 @@
 class Elemento {
   nome: string;
-	costo: number;
+  costo: number;
+  quantita: number;
 
-	constructor(nome: string, costo:number) {
-        this.nome = nome;
-		this.costo = costo;
-    }
+  constructor(nome: string, costo:number, quantita: number) {
+    this.nome = nome;
+    this.costo = costo;
+    this.quantita = quantita;
+  }
 
-	visualizza() {
-        return this.nome +" - " + this.costo;
-    }
+  visualizza() {
+    return "nome: "+ this.nome +" | costo: " + this.costo +" | quantita: "+ this.quantita;
+  }
+
+  aggiungi() {
+    this.quantita = this.quantita +1;
+  }
 }
 
 enum LavoratoreTipo { Manovale, Muratore, Carpentiere, Elettricista, Idraulico, Preposto, Direttore, Architetto, Ingegnere};
 
 class Lavoratore extends Elemento {
-	tipologia : LavoratoreTipo;
+  tipologia : LavoratoreTipo;
 
-	constructor(nome: string, costo: number, tipologia: LavoratoreTipo)
-	{
-		super(nome, costo);
-		this.tipologia = tipologia;
-	}
+  constructor(nome: string, costo: number, quantita: number, tipologia: LavoratoreTipo)
+  {
+    super(nome, costo, quantita);
+    this.tipologia = tipologia;
+  }
 
-	visualizza() {
-		return this.nome +" - " + this.costo +" - "+ LavoratoreTipo[this.tipologia];
-	}
+  visualizza() {
+    return super.visualizza() + " | tipologia: " + LavoratoreTipo[this.tipologia];
+    //return this.nome +" - " + this.costo +" - "+ LavoratoreTipo[this.tipologia];
+  }
 }
 
-var emilie:Elemento = new Elemento("Emilie", 10.50);
-var emilieLav:Lavoratore = new Lavoratore("Emilie", 10.50, LavoratoreTipo.Architetto);
+var emilie:Elemento = new Elemento("Emilie", 10.50, 1);
+var emilieLav:Lavoratore = new Lavoratore("Emilie", 10.50, 1, LavoratoreTipo.Architetto);
 
 // let button = document.createElement('button');
 // button.textContent = "Visualizza";
